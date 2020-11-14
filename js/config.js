69cd8c58a888bb6179bb7b8a238c53775d2d616c
Ailson Roberto <robertofeitosa@gmail.com>
2020-11-14 03:22:23 -0300

    Refatoracao - Melhorando layout

--- js/config.js
+++
@@ -1,9 +0,0 @@
-function setConfig() {
-    var texts = {
-        title: "Controle de Compras"
-    }
-    document.title = texts.title;
-    document.getElementById('navTitle').innerHTML = texts.title;
-}
-
-setConfig();
