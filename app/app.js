3bdbbfc5cbca592ac640015600040c4180695d97
Ailson Roberto <robertofeitosa@gmail.com>
2020-11-16 21:11:49 -0300

    Adicionando maskara no input de valor

--- app/app.js
+++ app/app.js
@@ -2,5 +2,6 @@ const seletor = document.querySelector.bind(document);
 
 const controller = new CompraController();
 
-seletor('#valor').addEventListener('change', Maskara.dinheiroMask);
 seletor('.form').addEventListener('submit', controller.adiciona.bind(controller));
+
+$('#valor').mask('000.000.000.000.000,00', {reverse: true});
